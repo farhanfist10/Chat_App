08313ff0b0f12cb897f24d6f5c250cf4af014e57
Farhan Akthar <farhanakthar2003@gmail.com>
2024-02-29 22:16:39 +0530

    2nd part completed

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,29 @@
+// import { BiLogOut } from "react-icons/bi";
+// import useLogout from "../../hooks/useLogout";
+
+// const LogoutButton = () => {
+//   const { loading, logout } = useLogout();
+
+//   return (
+//     <div className="mt-auto">
+//       {/* {!loading ? (
+//         <BiLogOut
+//           className="w-6 h-6 text-white cursor-pointer"
+//           onClick={logout}
+//         />
+//       ) : (
+//         <span className="loading loading-spinner"></span>
+//       )} */}
+//     </div>
+//   );
+// };
+// export default LogoutButton;
+import React from 'react'
+
+const LogoutButton = () => {
+  return (
+    <div>LogoutButton</div>
+  )
+}
+
+export default LogoutButton

25b36cd70d8f9cd83f6ffb03bbab239799f49a44
Farhan Akthar <farhanakthar2003@gmail.com>
2024-07-29 07:44:30 +0530

    first commit

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,5 +5,7 @@ import { getUserForSidebar } from "../controllers/user.controller.js";
 const router =express.Router();
 
 router.get("/",protectRoute,getUserForSidebar)
-
+router.get("/hello",(req,res)=>{
+    return "Hello World"
+})
 export default router;
